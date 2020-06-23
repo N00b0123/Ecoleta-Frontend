2b01877a977bcb98d2b6dcf19e10f7bebb2579d5
william grosseli <williamgrosseli@gmail.com>
2020-06-23 20:12:46 -0300

    implementacao inicial do mapa

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi'
+import { Map, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css';
 import logo from '../../assets/logo.svg'
@@ -49,6 +50,14 @@ const CreatePoint = () => {
                         <span>Selecione o endereço no mapa</span>
                     </legend>
 
+                    <Map center={[-27.6472809, -52.2796273]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[-27.6472809, -52.2796273]} />
+                    </Map>
+
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="uf">Estado (UF)</label>
@@ -64,7 +73,7 @@ const CreatePoint = () => {
                             </select>
                         </div>
                     </div>
- 
+
                 </fieldset>
 
                 <fieldset>
@@ -75,27 +84,27 @@ const CreatePoint = () => {
 
                     <ul className="items-grid">
                         <li>
-                            <img src="http://localhost:3333/uploads/organicos.svg" alt="teste"/>
+                            <img src="http://localhost:3333/uploads/organicos.svg" alt="teste" />
                             <span>Organico</span>
                         </li>
                         <li>
-                            <img src="http://localhost:3333/uploads/organicos.svg" alt="teste"/>
+                            <img src="http://localhost:3333/uploads/organicos.svg" alt="teste" />
                             <span>Organico</span>
                         </li>
                         <li className="selected">
-                            <img src="http://localhost:3333/uploads/organicos.svg" alt="teste"/>
+                            <img src="http://localhost:3333/uploads/organicos.svg" alt="teste" />
                             <span>Organico</span>
                         </li>
                         <li>
-                            <img src="http://localhost:3333/uploads/organicos.svg" alt="teste"/>
+                            <img src="http://localhost:3333/uploads/organicos.svg" alt="teste" />
                             <span>Organico</span>
                         </li>
                         <li>
-                            <img src="http://localhost:3333/uploads/organicos.svg" alt="teste"/>
+                            <img src="http://localhost:3333/uploads/organicos.svg" alt="teste" />
                             <span>Organico</span>
                         </li>
                         <li>
-                            <img src="http://localhost:3333/uploads/organicos.svg" alt="teste"/>
+                            <img src="http://localhost:3333/uploads/organicos.svg" alt="teste" />
                             <span>Organico</span>
                         </li>
                     </ul>
